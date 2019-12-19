bc093a13406119fa5faa1c1ba6180fb416eab05b
juan <jdgn94@gmail.com>
2019-12-19 13:53:12 -0400

    implementacion de un ejemplo de un swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hola mundo!!!!!'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Películas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperCards(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 15.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        itemWidth: 300.0,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+        layout: SwiperLayout.STACK,
       ),
     );
   }
